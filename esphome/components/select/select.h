93cbcee4ad868d8b0d06f5c742e0fc24d51e9d6d
Sam Hunt <sam@samhunt.co>
2023-08-26 22:52:19 +1000

    test including template_select in select

--- esphome/components/select/select.h
+++ esphome/components/select/select.h
@@ -61,6 +61,18 @@ class Select : public EntityBase {
 
   void add_on_state_callback(std::function<void(std::string, size_t)> &&callback);
 
+  void set_template(std::function<optional<std::string>()> &&f) { this->f_ = f; }
+
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
+
+  Trigger<std::string> *get_set_trigger() const { return this->set_trigger_; }
+  void set_optimistic(bool optimistic) { this->optimistic_ = optimistic; }
+  void set_initial_option(const std::string &initial_option) { this->initial_option_ = initial_option; }
+  void set_restore_value(bool restore_value) { this->restore_value_ = restore_value; }
+
  protected:
   friend class SelectCall;
 
@@ -74,6 +86,15 @@ class Select : public EntityBase {
 
   CallbackManager<void(std::string, size_t)> state_callback_;
   bool has_state_{false};
+  
+  void control(const std::string &value) override;
+  bool optimistic_ = false;
+  std::string initial_option_;
+  bool restore_value_ = false;
+  Trigger<std::string> *set_trigger_ = new Trigger<std::string>();
+  optional<std::function<optional<std::string>()>> f_;
+
+  ESPPreferenceObject pref_;
 };
 
 }  // namespace select
