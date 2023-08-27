8e289cb7b5b0dc189793d4ad2281d18f4d423fd9
Sam Hunt <sam@samhunt.co>
2023-08-27 13:38:17 +1000

    test

--- esphome/components/panasonic_ir/panasonic_ac_select.h
+++ esphome/components/panasonic_ir/panasonic_ac_select.h
@@ -6,7 +6,7 @@
 namespace esphome {
 namespace panasonic_ir {
 
-    class PanasonicACSelect : public select::Select, public PollingComponent {
+    class PanasonicACSelect : public select::Select {
         // protected:
         // void control(const std::string &value) override { this->publish_state(value); }
         };
