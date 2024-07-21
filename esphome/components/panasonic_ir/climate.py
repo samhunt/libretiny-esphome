ceb28997fdede625a552120c6f090365c7cad93f
Sam Hunt <sam@samhunt.co>
2024-07-21 12:16:22 +1000

    remove CONF_SUPPORTS_VERTICAL_SWING

--- esphome/components/panasonic_ir/climate.py
+++ esphome/components/panasonic_ir/climate.py
@@ -5,7 +5,7 @@ from esphome.const import (
     CONF_ID,
 #    CONF_SUPPORTS_BOTH_SWING,
 #    CONF_SUPPORTS_HORIZONTAL_SWING,
-    CONF_SUPPORTS_VERTICAL_SWING,
+#    CONF_SUPPORTS_VERTICAL_SWING,
 )
 
 AUTO_LOAD = ["climate_ir", "select"]
@@ -38,9 +38,9 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(PanasonicClimate),
 #        cv.Optional(CONF_SUPPORTS_HORIZONTAL_SWING, default=False): cv.boolean,
-        cv.Optional(CONF_SUPPORTS_VERTICAL_SWING, default=True): cv.boolean,
+#        cv.Optional(CONF_SUPPORTS_VERTICAL_SWING, default=True): cv.boolean,
 #        cv.Optional(CONF_SUPPORTS_BOTH_SWING, default=False): cv.boolean,
-        cv.Optional(CONF_HORIZONTAL_SWING_SELECT): SELECT_SCHEMA,
+#        cv.Optional(CONF_HORIZONTAL_SWING_SELECT): SELECT_SCHEMA,
         cv.Optional(CONF_VERTICAL_SWING_SELECT): SELECT_SCHEMA,
     }
 )
@@ -53,7 +53,7 @@ async def to_code(config):
     cg.add(
         var.set_supported_swing_modes(
 #            config[CONF_SUPPORTS_HORIZONTAL_SWING],
-            config[CONF_SUPPORTS_VERTICAL_SWING],
+#            config[CONF_SUPPORTS_VERTICAL_SWING],
 #            config[CONF_SUPPORTS_BOTH_SWING],
         )
     )
